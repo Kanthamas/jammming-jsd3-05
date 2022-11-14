d6bf9a989766d11ad84ff5d1c6bcacbfaa92d873
auntieMarch <auntiemarch@gmail.com>
2022-11-14 15:06:04 +0700

    feature: add search(term) , handleTermChange(event)

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,30 @@ import React from "react";
 import "./SearchBar.css";
 
 export default class SearchBar extends React.Component {
+	/// constructor part ///
+	constructor(props) {
+		super(props);
+		this.search = this.search.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
+	}
+
+	/// logic part ///
+	search(term) {
+		this.props.onSearch(term);
+	}
+
+	handleTermChange(event) {
+		this.search(event.target.value);
+	}
+
+	/// render part ///
 	render() {
 		return (
 			<div className='SearchBar'>
-				<input placeholder='Enter A Song, Album, or Artist' />
+				<input
+					placeholder='Enter A Song, Album, or Artist'
+					onChange={this.handleTermChange}
+				/>
 				<button className='SearchButton'>SEARCH</button>
 			</div>
 		);
