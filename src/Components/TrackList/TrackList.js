2805389f4099cc2c641c8a988ee7f53cbbdcd0b5
auntieMarch <auntiemarch@gmail.com>
2022-11-14 13:59:54 +0700

    feature: add <Track  onRemove, isRemove/>

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,15 @@ export default class TrackList extends React.Component {
 		return (
 			<div className='TrackList'>
 				{this.props.tracks.map((track) => {
-					return <Track key={track.id} track={track} />;
+					return (
+						<Track
+							key={track.id}
+							track={track}
+							onAdd={this.props.onAdd}
+							onRemove={this.props.onRemove}
+							isRemove={this.props.isRemove}
+						/>
+					);
 				})}
 			</div>
 		);
