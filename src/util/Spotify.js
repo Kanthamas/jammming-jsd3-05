41d5b8f66d5d4d01de2e9d8d47f64ff14146f4c0
auntieMarch <auntiemarch@gmail.com>
2022-11-14 17:45:13 +0700

    feature: check access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,10 +2,34 @@
 // Send a search request to the Spotify API
 // Save a user’s playlist to their Spotify account
 
-const usesrAccessToken = "";
+const CLIENT_ID = "5d1daae238404fccbba761524037653a";
+const REDIRECT_URI = "http://localhost:3000/";
+
+const SPOTIFY_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+
+let accessToken;
+let expiresIn;
 
 export const Spotify = {
 	getAccessToken() {
-		if (usesrAccessToken) return usesrAccessToken;
+		//// Case 1 (step 78): if the user’s access token is already set. If it is, return the value saved to access token.
+
+		if (accessToken) return accessToken;
+
+		//// Case 2 (step 79): If the access token is not already set, check the URL to see if it has just been obtained.
+
+		const urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
+		const urlExpirationIn = window.location.href.match(/expires_in=([^&]*)/);
+		if (urlAccessToken && urlExpirationIn) {
+			accessToken = urlAccessToken[1];
+			expiresIn = urlAccessToken[1];
+			window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+			window.history.pushState("Access Token", null, "/");
+			return accessToken;
+		} else {
+			//// Case 3 (step 81): if access token variable is empty and is not in the URL
+
+			return window.location(SPOTIFY_URL);
+		}
 	},
 };
