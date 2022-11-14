e2c42d4aab5bb13629da6cfae899fcf832e451a1
auntieMarch <auntiemarch@gmail.com>
2022-11-14 11:11:56 +0700

    chore: change from function to class

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Playlist.css";
 
-function Playlist() {
-	return (
-		<div className='Playlist'>
-			<input value='New Playlist' />
-			{/* <!-- Add a TrackList component --> */}
-			<button className='Playlist-save'>SAVE TO SPOTIFY</button>
-		</div>
-	);
+export default class Playlist extends React.Component {
+	render() {
+		return (
+			<div className='Playlist'>
+				<input value='New Playlist' />
+				{/* <!-- Add a TrackList component --> */}
+				<button className='Playlist-save'>SAVE TO SPOTIFY</button>
+			</div>
+		);
+	}
 }
-
-export default Playlist;
