aab06faa87b234306d4c3bffdb2170ebd2287b8e
auntieMarch <auntiemarch@gmail.com>
2022-11-14 14:01:56 +0700

    feature: add rempveTrack()

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,14 +2,31 @@ import React from "react";
 import "./Track.css";
 
 export default class Track extends React.Component {
+	/// constructor part///
+	constructor(props) {
+		super(props);
+		this.addTrack = this.addTrack.bind(this);
+	}
+	/// logic part ///
+	addTrack() {
+		this.props.onAdd(this.props.track);
+	}
+
+	removeTrack() {
+		this.props.onRemove(this.props.track);
+	}
+
+	/// render part///
 	render() {
 		return (
 			<div className='Track'>
 				<div className='Track-information'>
-					{/* <h3><!-- track name will go here --></h3> */}
-					{/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+					<h3>{this.props.track.name}</h3>
+					<p>
+						{this.props.track.artist} | {this.props.track.album}
+					</p>
 				</div>
-				{/* <button class="Track-action"><!-- + or - will go here --></button> */}
+				{/* {/* <button class="Track-action"><!-- + or - will go here --></button> */}
 			</div>
 		);
 	}
