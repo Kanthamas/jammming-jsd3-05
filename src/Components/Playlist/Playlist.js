0b03d3dcb1866c0ed8b829df7c6a1ffa54c15562
auntieMarch <auntiemarch@gmail.com>
2022-11-14 14:51:02 +0700

    feature: onClick={this.props.onSave}

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,16 +2,30 @@ import React from "react";
 import "./Playlist.css";
 import TrackList from "../TrackList/TrackList";
 export default class Playlist extends React.Component {
+	/// constructor part ///
+	constructor(props) {
+		super(props);
+		this.handleNameChange = this.handleNameChange.bind(this);
+	}
+
+	/// logic part ///
+	handleNameChange(event) {
+		this.props.handleNameChange(event.target.value);
+	}
+
+	/// render part ///
 	render() {
 		return (
 			<div className='Playlist'>
-				<input value='New Playlist' />
+				<input value='New Playlist' onChange={this.handleNameChange} />
 				<TrackList
 					tracks={this.props.playlistTracks}
 					onRemove={this.props.onRemove}
 					isRemove={true}
 				/>
-				<button className='Playlist-save'>SAVE TO SPOTIFY</button>
+				<button className='Playlist-save' onClick={this.props.onSave}>
+					SAVE TO SPOTIFY
+				</button>
 			</div>
 		);
 	}
